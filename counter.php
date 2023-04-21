1110e0ecde9088b8210b241e95cfc9cb6483b8b1
Hyunji Kim <papertoy01@gmail.com>
2023-04-21 15:01:45 +0900

    방문횟수 카운트 최종본

--- counter.php
+++ counter.php
@@ -4,7 +4,7 @@
     }
 </style>
 <?php
-    $filename = "counter.text"; //방문 횟수를 저장할 파일
+    $filename = "counter.txt"; //방문 횟수를 저장할 파일
     $count = 0; //방문 횟수
 
     if(file_exists($filename)){ // 
@@ -13,7 +13,7 @@
     
     $count++; //방문횟수를 증가시킨다.
 
-    file_put_contents($file, $count);
+    file_put_contents($filename, $count);
 
     echo "<h1>당신은 ".$count."번째 방문하셨습니다.</h1>";
 ?>
