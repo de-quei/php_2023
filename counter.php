58a590983268c4c81fe7a619bfc7508850e600c7
Hyunji Kim <108276366+de-quei@users.noreply.github.com>
2023-04-21 15:05:35 +0900

    주석 추가

--- counter.php
+++ counter.php
@@ -5,15 +5,15 @@
 </style>
 <?php
     $filename = "counter.txt"; //방문 횟수를 저장할 파일
-    $count = 0; //방문 횟수
+    $count = 0; //초기 방문 횟수.
 
-    if(file_exists($filename)){ // 
+    if(file_exists($filename)){ //파일이 존재하면, 파일에서 방문횟수를 읽어온다.
         $count = file_get_contents($filename);
     }
     
     $count++; //방문횟수를 증가시킨다.
 
-    file_put_contents($filename, $count);
+    file_put_contents($filename, $count); //방문횟수를 파일에 저장한다.
 
     echo "<h1>당신은 ".$count."번째 방문하셨습니다.</h1>";
 ?>
