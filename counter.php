7c6ba8a4f828255ace779b006f8ca7e503fd83ca
Hyunji Kim <papertoy01@gmail.com>
2023-04-21 14:59:27 +0900

    방문횟수

---
+++ counter.php
@@ -0,0 +1,19 @@
+<style>
+    h1{
+        color : red;
+    }
+</style>
+<?php
+    $filename = "counter.text"; //방문 횟수를 저장할 파일
+    $count = 0; //방문 횟수
+
+    if(file_exists($filename)){ // 
+        $count = file_get_contents($filename);
+    }
+    
+    $count++; //방문횟수를 증가시킨다.
+
+    file_put_contents($file, $count);
+
+    echo "<h1>당신은 ".$count."번째 방문하셨습니다.</h1>";
+?>
